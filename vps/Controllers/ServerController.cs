6d74a69179ef231b15409fd68e2e110c9aa1760e
Maksim Harbacheuski <gorbachewski.m@gmail.com>
2023-01-10 22:40:07 +0300

    first

---
+++ vps/Controllers/ServerController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using vps.Models.Server;
+
+namespace vps.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class ServerController : ControllerBase
+    {
+        public ILogger<ServerController> Logger { get; set; }
+
+        public ServerController(ILogger<ServerController> logger) 
+        {
+            Logger = logger;
+        }
+
+        [HttpPost]
+        public bool Create(ServerCreateModel model)
+        {
+            using var process = Process.Start(new ProcessStartInfo
+            {
+                FileName = "/bin/sh",
+                RedirectStandardInput = true,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                WorkingDirectory = "/home/max/ubuntu_ssh"
+            });
+
+            if (process == null)
+                return false;
+
+            using (var sw = process.StandardInput)
+            {
+                if (sw.BaseStream.CanWrite)
+                {
+                    sw.WriteLine($"docker run --name {model.Username} -p 2222:22 -d test tail -f /dev/null");
+                    sw.WriteLine($"useradd -ms /bin/sh {model.Username}");
+                    sw.WriteLine($"usermod -aG /bin/bash {model.Username}");
+                    sw.WriteLine(string.Format("echo -e \"{1}\\n{1}\" | passwd {0}", model.Username, model.Password));
+                    sw.WriteLine($"cd /home/{model.Username}");
+                    sw.WriteLine("mkdir .ssh && chmod 700 .ssh");
+                    sw.WriteLine("touch .ssh/authorized_keys && chmod 600 .ssh/authorized_keys");
+                    sw.WriteLine(string.Format("chown -R {0}:{0} /home/{0}", model.Username));
+                    sw.WriteLine("service ssh start");
+                }
+            }
+
+            process.WaitForExit();
+            var output = process.StandardOutput.ReadToEnd();
+            var error = process.StandardError.ReadToEnd();
+
+            Logger.LogInformation($"try to create server. username {model.Username}, passwd {model.Password}, output: {output}, error {error}");
+
+            return process.ExitCode.Equals(0);
+        }
+    }
+}
---
+++ vps/Models/Server/ServerCreateModel.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace vps.Models.Server
+{
+    public class ServerCreateModel
+    {
+        [JsonProperty("username")]
+        public string Username { get; set; }
+
+        [JsonProperty("password")]
+        public string Password { get; set; }
+    }
+}
